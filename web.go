60d2ae737595a5d78c97c4ad904f7e3df1219fce
Ahmad <ahmad@schlegel.(none)>
2014-01-22 10:00:15 -0500

    deploy test

--- web.go
+++ web.go
@@ -3,5 +3,33 @@
 // Tue Jan 21, 2014
 package web
 
+import (
+    "io/ioutil"
+    "log"
+    "net/http"
+    "appengine"
+)
+
+const (
+    PATH = "index.html"
+)
+
+var (
+    Data []byte
+    err error
+)
+
 func init() {
+    // cache single page html webapp
+    Data, err = ioutil.ReadFile(PATH)
+    if err != nil {
+        log.Fatal(err)
+    }
+    // http handler
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write(Data)
+        // logging
+        c := appengine.NewContext(r)
+        c.Infof("okaq.funka: hello %s", r.RemoteAddr)
+    })
 }
